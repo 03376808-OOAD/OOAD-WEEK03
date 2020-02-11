fe4982e76c6943d1b864ce18e3875783949585fa
AvirutRut <42264185+AvirutRut@users.noreply.github.com>
2020-02-11 16:43:02 +0700

    2

--- Lab submits/60030112/Activity 2/Encapsulation/Encapsulation/Program.cs
+++ Lab submits/60030112/Activity 2/Encapsulation/Encapsulation/Program.cs
@@ -10,13 +10,19 @@ namespace Encapsulation
     {
         static void Main(string[] args)
         {
-            Student su = new Student();
-            su.Name = "Student Name";
-            su.ID = "12345678";
-            su.GPA = 3.5f;
-            Console.WriteLine("Student name : " + su.Name);
-            Console.WriteLine("Student ID   : " + su.ID);
-            Console.WriteLine("Student GPA  : " + su.GPA);
+            Student su = new Student("Student Name", "12345678");
+            try
+            {
+                //su.Name = "My name";
+                su.GPA = 4.0f;
+                Console.WriteLine("Student name : " + su.Name);
+                Console.WriteLine("Student ID   : " + su.ID);
+                Console.WriteLine("Student GPA  : " + su.GPA);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
@@ -25,20 +31,34 @@ namespace Encapsulation
         private string name;
         private string id;
         private float gpa;
+
+        public Student(string Name, string ID)
+        {
+            this.name = Name;
+            this.id = ID;
+        }
+
         public string Name
         {
             get { return name; }
-            set { name = value; }
         }
         public string ID
         {
             get { return id; }
-            set { id = value; }
         }
         public float GPA
         {
-            get { return gpa; }
-            set { gpa = value; }
+            get
+            {
+                return gpa;
+            }
+            set
+            {
+                if (value > 0.0 && value <= 4.0)
+                    gpa = value;
+                else
+                    throw (new Exception("Error!!!! invalid GPA"));
+            }
         }
     }
 }
