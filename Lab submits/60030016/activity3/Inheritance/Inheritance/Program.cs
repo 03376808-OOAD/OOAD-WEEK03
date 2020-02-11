5c15f9c3a7882b25dfb1fa089c3a3454f16f2023
JirawatChairongsri <42440557+JirawatChairongsri@users.noreply.github.com>
2020-02-11 17:10:32 +0700

    R2

---
+++ Lab submits/60030016/activity3/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher teacher = new Teacher("Tom", 350f);
+            // teacher work for 20Hr/month
+            Console.WriteLine("{0} charge = {1}", teacher.TypeName(), teacher.CalculateCharge(20f));
+            Console.ReadLine();
+        }
+    }
+    class Teacher
+    {
+        // constructor (for initial private/protected variables)
+        public Teacher(string name, float billingRate)
+        {
+            this.name = name;
+            this.billingRate = billingRate;
+        }
+        // figure out the charge based on teacher's rate
+        public float CalculateCharge(float hours)
+        {
+            return (hours * billingRate);
+        }
+        // return the name of this type
+        public string TypeName()
+        {
+            return ("Teacher");
+        }
+        private string name;
+        protected float billingRate;
+    }
+}
