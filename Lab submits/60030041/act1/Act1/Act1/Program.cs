0d6c907b61ced7e1fd2555094c60dc807f1f4053
tanakorn128 <42264080+tanakorn128@users.noreply.github.com>
2020-02-11 16:15:04 +0700

    act1

---
+++ Lab submits/60030041/act1/Act1/Act1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Act1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point myPoint = new Point();
+            myPoint.X = 10;
+            myPoint.Y = 10;
+            Console.WriteLine("myPoint.X = {0}", myPoint.X);
+            Console.WriteLine("myPoint.Y = {0}", myPoint.Y);
+            Console.ReadLine();
+        }
+    }
+}
+class Point
+{
+    private int x;
+    private int y;
+
+    public int X
+    {
+        get { return x; }
+        set { x = value; }
+    }
+
+    public int Y
+    {
+       // get { return y; }
+        set { y = value; }
+    }
+}
