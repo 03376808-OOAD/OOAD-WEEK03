564bd7efa19d4653a89ddfb4c3670ea38d425d07
thanaponyalan <thanaponyalan@gmail.com>
2020-02-11 18:17:07 +0700

    Latest VS Files

--- Lab submits/60030037/LabWk3-Act4/LabWk3-Act4/Program.cs
+++ Lab submits/60030037/LabWk3-Act4/LabWk3-Act4/Program.cs
@@ -18,13 +18,65 @@ namespace LabWk3_Act4
         }
         static void Main(string[] args)
         {
-            Program p = new Program();
-            p.print(5);
-            p.print((5.26f).ToString());
-            p.print(264.2871);
-            p.print((int)1354.23);
-            p.print("Hello World of C#");
+            Person p = new Person();
+            p.SayHi();
+            Student s = new Student();
+            s.SayHi();
+            Person sp = new Student();
+            sp.SayHi();
+            Teacher t = new Teacher();
+            t.SayHi();
+            Person tp = new Teacher();
+            tp.SayHi();
             Console.ReadKey();
+        }
+    }
+
+    class Vector
+    {
+        public Vector(int x,int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("({0},{1})", X, Y);
+        }
+        public static Vector operator +(Vector v1,Vector v2)
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
+        }
+        public static Vector operator -(Vector v1)
+        {
+            return new Vector(-v1.X, -v1.Y);
+        }
+    }
+
+    class Person
+    {
+        public virtual void SayHi()
+        {
+            Console.WriteLine("Hi I'm a person");
+        }
+    }
+
+    class Student : Person
+    {
+        public override void SayHi()
+        {
+            Console.WriteLine("Hi I'm a student");
+        }
+    }
+
+    class Teacher : Person
+    {
+        public new void SayHi()
+        {
+            Console.WriteLine("Hi I'm a teacher");
         }
     }
 }
