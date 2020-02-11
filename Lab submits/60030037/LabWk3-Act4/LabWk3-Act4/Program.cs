f8f07fa4e4947d78690d9cd1c4174197fba7672a
thanaponyalan <thanaponyalan@gmail.com>
2020-02-11 17:28:46 +0700

    VS Files : act4

---
+++ Lab submits/60030037/LabWk3-Act4/LabWk3-Act4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace LabWk3_Act4
+{
+    class Program
+    {
+        void print(int i)
+        {
+            Console.WriteLine("Printing int: {0}", i);
+        }
+        void print(double f)
+        {
+            Console.WriteLine("Printing float: {0}", f);
+        }
+        void print(string s)
+        {
+            Console.WriteLine("Printing string: {0}", s);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.print(5);
+            p.print((5.26f).ToString());
+            p.print(264.2871);
+            p.print((int)1354.23);
+            p.print("Hello World of C#");
+            Console.ReadKey();
+        }
+    }
+}
