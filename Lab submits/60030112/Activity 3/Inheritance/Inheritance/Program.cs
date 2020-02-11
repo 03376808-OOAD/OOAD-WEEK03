b968d55ea41a3aaa7b4e26b9bfb21b31b92cc815
AvirutRut <42264185+AvirutRut@users.noreply.github.com>
2020-02-11 17:03:48 +0700

    modify

---
+++ Lab submits/60030112/Activity 3/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Teacher teacher = new Teacher("Tom", 350f);
+            Console.WriteLine("{0} charge = {1}", teacher.TypeName(), teacher.CalculateCharge(20f));
+            Console.ReadLine();
+        }
+    }
+    class Teacher
+    {
+        public Teacher(string name, float billingRate)
+        {
+            this.name = name;
+            this.billingRate = billingRate;
+        }
+        public float CalculateCharge(float hours)
+        {
+            return (hours * billingRate);
+        }
+        public string TypeName()
+        {
+            return ("Teacher");
+        }
+        private string name;
+        protected float billingRate;
+    }
+}
