1ed2b5f13f7c4aa7c8568b6b15c9dec5ee377829
JirawatChairongsri <42440557+JirawatChairongsri@users.noreply.github.com>
2020-02-11 16:36:00 +0700

    R1

---
+++ Lab submits/60030016/activity1/A1/A1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace A1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point myPoint = new Point();  
+            myPoint.X = 10;
+            myPoint.Y = 10;
+            Console.WriteLine("myPoint.x = {0}", myPoint.X);
+            Console.WriteLine("myPoint.y = {0}", myPoint.Y);
+            Console.ReadLine();
+        }
+    }
+
+    class Point          
+    {
+        private int x;    
+        private int y;
+
+        public int X      
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+    }
+}
