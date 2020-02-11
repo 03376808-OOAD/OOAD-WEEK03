ad9ace8e07b5c1b40a7948f209c6a378f08fde3e
nantikan tippawan <60030049@kmitl.ac.th>
2020-02-11 23:27:17 +0700

    p

---
+++ Lab submits/60030049/Practices/Practices/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practices
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person t = new Teacher();
+            t.SayHi();
+            Console.ReadKey();
+        }
+    }
+    class Person
+    {
+        public virtual void SayHi()
+        {
+            Console.WriteLine("Hi Person");
+        }
+    }
+    class Student:Person
+    {
+        public override void SayHi()
+        {
+            Console.WriteLine("Student");
+        }
+    }
+    class Teacher:Person
+    {
+        public override void SayHi()
+        {
+            Console.WriteLine("Teacher");
+        }
+    }
+    class Professor : Teacher
+    {
+        public new void SayHi()
+        {
+            Console.WriteLine("Professor");
+        }
+    }
+}
