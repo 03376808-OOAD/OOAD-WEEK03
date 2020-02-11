f13a324437bbfa0428f0b987a2b923355718ebcd
tanakorn128 <42264080+tanakorn128@users.noreply.github.com>
2020-02-11 17:14:15 +0700

    act3

---
+++ Lab submits/60030041/act3/Act3/Act3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Act3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+class Tescher
+{
+    public Teacher(string name, float billingRate)
+    {
+
+    }
+}
