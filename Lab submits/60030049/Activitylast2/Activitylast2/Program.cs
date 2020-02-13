1ee2169b02849c38b8d640a5bfa1afb83b077130
nantikan tippawan <60030049@kmitl.ac.th>
2020-02-13 23:15:21 +0700

    ac2

---
+++ Lab submits/60030049/Activitylast2/Activitylast2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activitylast2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TraineeStudent t = new TraineeStudent("60030049", 500);
+
+            try
+            {
+                Console.WriteLine("Student ID : "+ t.getStudentID);
+                Console.WriteLine("Salary : "+t.getSalary +" Bath");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+    class TraineeStudent
+    {
+        private string studentid;
+        private int salary;
+
+        public TraineeStudent(string getStudentID,int getSalary)
+        {
+            this.studentid = getStudentID;
+            this.salary = getSalary;
+        }
+        public string getStudentID
+        {
+            get { return studentid; }
+        }
+        public int getSalary
+        {
+            get
+            {
+                return salary;
+            }
+            /***
+            set
+            {
+                if (value <= 450)
+                    salary = value;
+                else
+                    throw (new Exception("Error!!! invalid Salary"));
+            }
+            */
+        }
+    }
+}
