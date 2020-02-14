3045eb2335369ce31f1eaa45a531cf6a5bbf3a56
AvirutRut <42264185+AvirutRut@users.noreply.github.com>
2020-02-14 18:03:44 +0700

    6

--- Lab submits/60030112/Activity 3/Inheritance/Inheritance/Program.cs
+++ Lab submits/60030112/Activity 3/Inheritance/Inheritance/Program.cs
@@ -11,7 +11,11 @@ namespace Inheritance
         static void Main(string[] args)
         {
             Teacher teacher = new Teacher("Tom", 350f);
-            Console.WriteLine("{0} charge = {1}", teacher.TypeName(), teacher.CalculateCharge(20f));
+            Professor prof = new Professor("Jerry", 500f, 5000f);
+            Console.WriteLine("{0} charge = {1}", teacher.TypeName(),
+                teacher.CalculateCharge(20f));
+            Console.WriteLine("{0} charge = {1}", prof.TypeName(),
+                prof.CalculateCharge(12F));
             Console.ReadLine();
         }
     }
@@ -32,5 +36,30 @@ namespace Inheritance
         }
         private string name;
         protected float billingRate;
+    }
+    class Professor : Teacher
+    {
+        private float emolument;  
+
+        public Professor(string name, float billingRate) : base(name, billingRate)
+        {
+        }
+
+        public Professor(string name, float billingRate, float emolument)
+        : this(name, billingRate)
+        {
+            this.emolument = emolument;
+        }
+
+        public new float CalculateCharge(float hours)
+        {
+            if (hours < 1.0F)
+                hours = 1.0F; 
+            return (hours * billingRate) + emolument;
+        }
+        public new string TypeName()
+        {
+            return ("Professor");
+        }
     }
 }
