e09f1eb94f1284cc79dbf028f876d2fb87e95a9e
AvirutRut <42264185+AvirutRut@users.noreply.github.com>
2020-02-14 18:21:59 +0700

    go

---
+++ Lab submits/60030112/Activity 4/Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            p.SayHi();
+            Student s = new Student();
+            s.SayHi();
+            Person sp = new Student();
+            sp.SayHi();
+            Teacher t = new Teacher();
+            t.SayHi();
+            Person tp = new Teacher();
+            tp.SayHi();
+            Console.ReadKey();
+        }
+    }
+    class Vector
+    {
+        public Vector(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public int X { get; set; }  
+        public int Y { get; set; }  
+
+        public override string ToString()
+        {
+            return string.Format("({0},{1})", X, Y);
+        }
+        public static Vector operator +(Vector v1, Vector v2) 
+        {
+            return new Vector(v1.X + v2.X, v1.Y + v2.Y);
+        }
+        public static Vector operator -(Vector v1)  
+        {
+            return new Vector(-v1.X, -v1.Y);
+        }
+    }
+    class Person
+    {
+        public virtual void SayHi()
+        {
+            Console.WriteLine("Hi I'm a person");
+        }
+    }
+
+    class Student : Person
+    {
+        public override void SayHi()
+        {
+            Console.WriteLine("Hi I'm a student");
+        }
+    }
+
+    class Teacher : Person
+    {
+        public new void SayHi()
+        {
+            Console.WriteLine("Hi I'm a teacher");
+        }
+    }
+}
